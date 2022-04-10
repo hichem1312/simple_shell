6065ffe1592a2c53864327424d79eaa7fb463832
hichem <hichemmaatoug55@gmail.com>
2022-04-09 20:39:49 -0700

    header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef header_h
+#define header_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/wait.h>
+extern char **environ;
+
+
+#endif
