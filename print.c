597d81eceb432176b2e929177df12004e6660d42
hichem <hichemmaatoug55@gmail.com>
2022-04-10 17:14:10 -0700

    printenviron

---
+++ print.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_environ - v
+ *Retun: environ
+ */
+int print_environ(void)
+{
+int i;
+for (i = 0; (environ[i]) != NULL; i++)
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+return (0);
+}
