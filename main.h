b8e63ebf6d9b97b98433def1d66341b32e6a0151
hichem <hichemmaatoug55@gmail.com>
2022-04-13 15:14:02 -0700

    header file

--- main.h
+++ main.h
@@ -7,6 +7,14 @@
 #include <string.h>
 #include <sys/wait.h>
 extern char **environ;
-
+int main(void)
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int execute(char **commande);
+char *getting_a_line();
+int print_environ(void);
+char **split_line(char *line);
 
 #endif
